0fd754dd4639f36c3cc2c76c7ad5969c0a7b29b9
edimilson braz <edimilson.gt8@gmail.com>
2024-01-31 08:21:59 -0300

    :sparkles: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,20 @@
-import Header from "../_components/header";
+import { format } from 'date-fns'
+import Header from '../_components/header'
+import { ptBR } from 'date-fns/locale'
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className='px-5 pt-5'>
+        <h2 className="text-xl font-bold">Olá Edimilson!</h2>
+        <p className="capitalize">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR
+          })}
+        </p>
+      </div>
     </div>
-  );
+  )
 }
