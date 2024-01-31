588929507bdef615ebd9ed4243eb59f0b9c82368
edimilson braz <edimilson.gt8@gmail.com>
2024-01-31 08:34:18 -0300

    :sparkles: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,24 @@
 import { format } from 'date-fns'
 import Header from '../_components/header'
 import { ptBR } from 'date-fns/locale'
+import Search from './_components/search'
 
 export default function Home() {
   return (
     <div>
       <Header />
 
-      <div className='px-5 pt-5'>
+      <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá Edimilson!</h2>
         <p className="capitalize">
           {format(new Date(), "EEEE',' dd 'de' MMMM", {
             locale: ptBR
           })}
         </p>
+      </div>
+
+      <div className='px-5 mt-6'>
+        <Search />
       </div>
     </div>
   )
