2d819dc667b46f91a160f76b5edfa7ef8ef0d7ac
edimilson braz <edimilson.gt8@gmail.com>
2024-02-01 09:41:38 -0300

    :sparkles: add initial barbershopItem component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,8 +3,13 @@ import Header from '../_components/header'
 import { ptBR } from 'date-fns/locale'
 import Search from './_components/search'
 import BookingItem from '../_components/booking-item'
+import BarbershopItem from './_components/barbershop-item'
+import { db } from '../_lib/prisma'
 
-export default function Home() {
+export default async function Home() {
+  // chamar prisma e pegar barbearias
+  const barbershops = await db.barbershop.findMany({})
+
   return (
     <div>
       <Header />
@@ -25,6 +30,18 @@ export default function Home() {
       <div className="px-5 mt-6">
         <h2 className='text-sm font-bold text-gray-400 uppercase mb-3'>Agendamentos</h2>
         <BookingItem />
+      </div>
+
+      <div className="mt-6">
+        <h2 className='text-sm px-5 font-bold text-gray-400 uppercase mb-3'>Recomendados</h2>
+        
+        <div className='flex gap-2 overflow-x-auto [&::-webkit-scrollbar]:hidden'>
+          {barbershops.map((barbershop) => (
+            <div key={barbershop.id} className='px-4'>
+              <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
+            </div>
+          ))}
+        </div>
       </div>
 
     </div>
