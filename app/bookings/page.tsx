150302d64555d2ce1339ca6c0ac363553d5cbdbf
edimilson braz <edimilson.gt8@gmail.com>
2024-02-19 08:38:27 -0300

    :bug: ensure auth options are exported from _lib folder

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,10 +1,9 @@
 import { getServerSession } from 'next-auth'
-import { authOptions } from '../api/auth/[...nextauth]/route'
 import Header from '../_components/header'
 import { redirect } from 'next/navigation'
 import { db } from '../_lib/prisma'
 import BookingItem from '../_components/booking-item'
-import { isFuture, isPast } from 'date-fns'
+import { authOptions } from '../_lib/auth'
 
 const BookingsPage = async () => {
   // Recuperar a sessão do usuário (ver se ele está logado ou não)
